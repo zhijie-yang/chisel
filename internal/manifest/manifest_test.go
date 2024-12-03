94db7809aff31a6e06004bcc4dedf0000df7fead
Zhijie Yang <zhijie.yang@canonical.com>
2024-12-03 18:46:57 +0100

    chore(manifest_test): s|/dir/hardlink/file2|/dir/hardlink|

--- internal/manifest/manifest_test.go
+++ internal/manifest/manifest_test.go
@@ -37,7 +37,7 @@ var readManifestTests = []struct {
 		{"kind":"content","slice":"pkg1_myslice","path":"/dir/file"}
 		{"kind":"content","slice":"pkg1_myslice","path":"/dir/file2"}
 		{"kind":"content","slice":"pkg1_myslice","path":"/dir/foo/bar/"}
-		{"kind":"content","slice":"pkg1_myslice","path":"/dir/hardlink/file2"}
+		{"kind":"content","slice":"pkg1_myslice","path":"/dir/hardlink"}
 		{"kind":"content","slice":"pkg1_myslice","path":"/dir/link/file"}
 		{"kind":"content","slice":"pkg2_myotherslice","path":"/dir/foo/bar/"}
 		{"kind":"package","name":"pkg1","version":"v1","sha256":"hash1","arch":"arch1"}
@@ -45,7 +45,7 @@ var readManifestTests = []struct {
 		{"kind":"path","path":"/dir/file","mode":"0644","slices":["pkg1_myslice"],"sha256":"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855","final_sha256":"8067926c032c090867013d14fb0eb21ae858344f62ad07086fd32375845c91a6","size":21}
 		{"kind":"path","path":"/dir/file2","mode":"0644","slices":["pkg1_myslice"],"sha256":"b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c","size":3,"hard_link_id":1}
 		{"kind":"path","path":"/dir/foo/bar/","mode":"01777","slices":["pkg2_myotherslice","pkg1_myslice"]}
-		{"kind":"path","path":"/dir/hardlink/file2","mode":"0644","slices":["pkg1_myslice"],"sha256":"b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c","size":3,"hard_link_id":1}
+		{"kind":"path","path":"/dir/hardlink","mode":"0644","slices":["pkg1_myslice"],"sha256":"b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c","size":3,"hard_link_id":1}
 		{"kind":"path","path":"/dir/link/file","mode":"0644","slices":["pkg1_myslice"],"link":"/dir/file"}
 		{"kind":"path","path":"/manifest/manifest.wall","mode":"0644","slices":["pkg1_manifest"]}
 		{"kind":"slice","name":"pkg1_manifest"}
@@ -57,7 +57,7 @@ var readManifestTests = []struct {
 			{Kind: "path", Path: "/dir/file", Mode: "0644", Slices: []string{"pkg1_myslice"}, SHA256: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", FinalSHA256: "8067926c032c090867013d14fb0eb21ae858344f62ad07086fd32375845c91a6", Size: 0x15, Link: ""},
 			{Kind: "path", Path: "/dir/file2", Mode: "0644", Slices: []string{"pkg1_myslice"}, SHA256: "b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c", Size: 0x03, Link: "", HardLinkID: 0x01},
 			{Kind: "path", Path: "/dir/foo/bar/", Mode: "01777", Slices: []string{"pkg2_myotherslice", "pkg1_myslice"}, SHA256: "", FinalSHA256: "", Size: 0x0, Link: ""},
-			{Kind: "path", Path: "/dir/hardlink/file2", Mode: "0644", Slices: []string{"pkg1_myslice"}, SHA256: "b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c", Size: 0x03, Link: "", HardLinkID: 0x01},
+			{Kind: "path", Path: "/dir/hardlink", Mode: "0644", Slices: []string{"pkg1_myslice"}, SHA256: "b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c", Size: 0x03, Link: "", HardLinkID: 0x01},
 			{Kind: "path", Path: "/dir/link/file", Mode: "0644", Slices: []string{"pkg1_myslice"}, SHA256: "", FinalSHA256: "", Size: 0x0, Link: "/dir/file"},
 			{Kind: "path", Path: "/manifest/manifest.wall", Mode: "0644", Slices: []string{"pkg1_manifest"}, SHA256: "", FinalSHA256: "", Size: 0x0, Link: ""},
 		},
@@ -75,7 +75,7 @@ var readManifestTests = []struct {
 			{Kind: "content", Slice: "pkg1_myslice", Path: "/dir/file"},
 			{Kind: "content", Slice: "pkg1_myslice", Path: "/dir/file2"},
 			{Kind: "content", Slice: "pkg1_myslice", Path: "/dir/foo/bar/"},
-			{Kind: "content", Slice: "pkg1_myslice", Path: "/dir/hardlink/file2"},
+			{Kind: "content", Slice: "pkg1_myslice", Path: "/dir/hardlink"},
 			{Kind: "content", Slice: "pkg1_myslice", Path: "/dir/link/file"},
 			{Kind: "content", Slice: "pkg2_myotherslice", Path: "/dir/foo/bar/"},
 		},
